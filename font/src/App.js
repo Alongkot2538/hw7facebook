d7ca6d0180ca9f27eae2db8966872b82afc06859
alongkot2538 <alongkot628@gmail.com>
2020-03-19 12:48:43 +0700

    fisrt

---
+++ font/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Student from './components/Student'
+import Facebook from './components/Facebook';
+
+function App() {
+  return (
+    <div className="App">
+    <Facebook/>
+     {/* <Student/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ font/src/components/Facebook.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import FacebookLogin from 'react-facebook-login';
+import Student from './Student';
+
+export default class Facebook extends Component {
+
+    state = {
+        auth: false,
+        name: '',
+        picture: ''
+    };
+
+    responseFacebook = response => {
+        console.log(response);
+        if(response.status !== 'unknown')
+        this.setState({
+            auth: true,
+            name: response.name,
+            picture: response.picture.data.url
+        });
+        
+    }
+    responseLogout = response => {
+        this.setState({
+            auth:false
+        });
+    }
+
+    componentClicked = () => {
+        console.log('Facebook btn clicked');
+    }
+
+    render(){
+        let facebookData;
+
+        this.state.auth ?
+        facebookData = (
+            <div>
+                <div style={{
+                    width: '400px',
+                    margin: 'auto',
+                    background: '#f4f4f4',
+                    padding: '20px',
+                    color: '#000'
+                }}>
+                    <img src={this.state.picture} alt={this.state.name} />
+                    <h2>Welcome {this.state.name}!</h2>
+                    <button onClick = {this.responseLogout}>logout</button>
+                </div>
+                <Student/>
+                </div>
+
+            ) : 
+            facebookData = (<FacebookLogin
+                appId="633420224119159"
+                autoLoad={true}
+                fields="name,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook} />);
+
+        return (
+            <>
+                {facebookData}
+            </>
+        );
+    }
+}
